ca04a57b77745ada04f0c1a0b8ecfb6a2d0ca08a
TamHov <hovhannisyan.tamara92@gmail.com>
2021-12-14 20:21:31 +0400

    Added a comment

--- IFrame.py
+++ IFrame.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+#Added a comment
 driver = webdriver.Chrome()
 driver.get("https://courses.letskodeit.com/practice%E2%80%8B")
 driver.maximize_window()
